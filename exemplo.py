8c559507ca4f908199f161506e0b5d7c1fc7a11e
wooy-a <nikcastro2006@gmail.com>
2022-06-08 10:07:29 -0400

    04

--- exemplo.py
+++ exemplo.py
@@ -1,20 +1,41 @@
-class Pessoa:
+class Pessoa: #Classe base; pai; superclasse
     def __init__(self, nome: str, sobrenome: str, idade: int):
-        self.nome = nome
+        #self.nome = variavel // nome = parametro
+        self.nome = nome 
         self.sobrenome = sobrenome
         self.idade = idade
 
-    def exibir(self):
+    def infoP(self):
         print('Nome: ', self.nome, '\nSobrenome: ', self.sobrenome, '\nIdade: ', self.idade)
 
-class Empregado(Pessoa):
-    def __init__(self, matricula: int): 
+class Empregado(Pessoa): #Filho; subclasse
+    #deve ser passado os parametros da classe pai que serão usados na classe filho
+    def __init__(self, nome, sobrenome, idade: int, matricula: int): 
         self.matricula = matricula
+        
+            #super() está invocando parametro
+        super().__init__(nome, sobrenome, idade)
 
-class Chefe(Pessoa):
-    def __init__(self, matricula: int): 
+    def infoE(self):
+        print('Empregado\n')
+            #super() está invocando metodo
+        super().infoP()
+        print('Matrícula: ', self.matricula)
+
+class Chefe(Pessoa): #Filho; subclasse
+    def __init__(self, nome, sobrenome, idade: int, matricula: int): 
         self.matricula = matricula
+        super().__init__(nome, sobrenome, idade)
 
-p1 = Pessoa('nome', 'sobrenome', 16)
-p1 = Chefe(1)
-print(p1.exibir, p1.matricula)
+    def infoC(self):
+        print('Chefe\n')
+        super().infoP()
+        print('Matrícula: ', self.matricula)
+
+print('-'*30)
+p1 = Chefe('Maria', 'Silva', 23, 1)
+p1.infoC()
+print('-'*30)
+p2 = Empregado('João', 'Santos', 28, 1365)
+p2.infoE()
+print('-'*30)
